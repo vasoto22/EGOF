044028c0b8b826ed5fd251cca9b426602ef9b40d
vasoto22 <84492509+vasoto22@users.noreply.github.com>
2023-06-30 10:23:39 -0500

    Update youtube.api.ts

--- structural/proxy/src/youtube.api.ts
+++ structural/proxy/src/youtube.api.ts
@@ -2,14 +2,14 @@ import { IVideoProvider } from "./video-provider.interface";
 
 class YouTubeApi implements IVideoProvider {
     private playList: any = {
-        courses: [
+        videos: [
             'https://www.youtube.com/watch?v=DKNVMILZDks',
             'https://www.youtube.com/watch?v=Ph4SLROqSEQ',
             'https://www.youtube.com/watch?v=Tvs3r0TVcRI'
         ]
     };
 
-    async getPlayList(code: string): Promise<string[]> {
+    async getPlayList(videos: string): Promise<string[]> {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 const result = this.playList[code] || null;
